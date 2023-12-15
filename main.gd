93e9fd141b508853b343c50d04795ebcb6530ba6
Hunter <hedwards0822@gmail.com>
2023-12-15 14:57:29 -0700

    crosshair and other minor changes

--- main.gd
+++ main.gd
@@ -1,19 +1,11 @@
-extends CanvasLayer
+extends Node3D
 
-
-var wait_time = 3.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$response.hide()
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_blue_box_bro_interacted(body):
-	$response.show()
-	
-	
-	
